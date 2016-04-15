d222de9e1ecc0b6c8417424c893da5e50eb4a976
allywarner <allywarner@sci.utah.edu>
2016-04-15 14:19:35 -0600

    matvec omp

--- matvec.c
+++ matvec.c
@@ -5,6 +5,7 @@ void matvec(char* matrix, double* x, double* result, size_t dim) {
   unsigned int row, col, i, n;
   double* diagonal = (double*)malloc(sizeof(double)*dim);
 
+#pragma omp parallel for
   for(i=0;i<dim;i++) {
     result[i] = 0;
     diagonal[i] = 0;
@@ -20,6 +21,7 @@ void matvec(char* matrix, double* x, double* result, size_t dim) {
     exit(EXIT_FAILURE);
   }
 
+//#pragma omp parallel for -- fix reading
   for(i=0;i<n;i++) {
 
     fscanf(A,"%d %d\n",&row,&col);
@@ -37,6 +39,7 @@ void matvec(char* matrix, double* x, double* result, size_t dim) {
 
   }
 
+#pragma omp parallel for
   for(i=0;i<dim;i++)
     result[i]+=diagonal[i]*x[i];
 
@@ -48,6 +51,7 @@ void matvec(char* matrix, double* x, double* result, size_t dim) {
 double dot(double* x, double* y, size_t n){
   unsigned int i;
   double a=0.0;
+#pragma omp parallel for
   for(i=0;i<n;i++)
     a += x[i]*y[i];
   return a;
@@ -56,6 +60,7 @@ double dot(double* x, double* y, size_t n){
 double norm(double* x, size_t n){
   unsigned int i;
   double nor = 0.0;
+#pragma omp parallel for
   for(i=0;i<n;i++)
     nor+=x[i]*x[i];
   nor = sqrt(nor);
