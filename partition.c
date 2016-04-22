05a7bd8e791b3c35bcd235b2a1f0d2d872d8418b
rviertel2 <u0452198@utah.edu>
2016-04-22 17:29:36 -0600

    MPI parallelization added

--- partition.c
+++ partition.c
@@ -13,33 +13,35 @@ int main(int argc,char* argv[]) {
   MPI_Comm_size(world_comm, &world_size);
 
   int i, row, col, N, dim;
-  srand(time(NULL));  // seed random number generator
   char mstr[100];
 
   // read name of matrix from arg list
   if (argc > 1)
-    strcpy(mstr,argv[1]);
-  else {
+  strcpy(mstr,argv[1]);
+  else if (world_rank == 0){
     fprintf(stderr, "error: no matrix path provided\n");
     exit(EXIT_FAILURE);
   }
 
-  // open matrix file
   FILE* matrix = fopen(mstr,"r");
-  if (matrix == NULL){
+
+  // open matrix file
+  if (matrix == NULL && world_rank == 0){
     fprintf(stderr, "error: matrix path invalid\n");
     exit(EXIT_FAILURE);
   }
 
   //scan first row of matrix for metadata
   fscanf(matrix,"%d %d %d\n",&row,&col,&N);
-  if (row != col){
+
+  if (row != col && world_rank == 0){
     fprintf(stderr,"error: this is not a sqare matrix\n");
     exit(EXIT_FAILURE);
   }
   dim = row;
 
-  coord* A = (coord*)malloc(sizeof(coord)*2*N); //ajacency matrix coordinates
+  coord* A = (coord*)malloc(sizeof(coord)*N); //ajacency matrix coordinates
+
   //scan matrix coordinates and store in memory
   for(i=0;i<N;i++)
     fscanf(matrix,"%d %d\n",&A[i].row,&A[i].col);
@@ -52,11 +54,16 @@ int main(int argc,char* argv[]) {
 
 void partition(coord* A, size_t dim, size_t N, int comm_size, MPI_Comm comm ) {
 
+  int rank, size, color;
+  MPI_Comm new_comm;
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &size);
+  color = rank % 2;
+
   unsigned int i, j, l, T_size=ITER;
   int* diagonal = (int*)malloc(sizeof(int)*dim); // #nonzeros specified per row
   int* scanned = (int*)malloc(sizeof(int)*dim);
-  double* V = (double*)malloc(sizeof(double)*ITER*ITER);// blank workspace for eig function
-  double* q[ITER];          //Krylov space basis matrix
+  double* q[ITER]; //Krylov space basis matrix
   double* z = (double*)malloc(sizeof(double)*dim); //new vector in each lanczos iteration
   double a[ITER]; // diagonal of lanczos matrix
   double b[ITER]; // subdiagonal of lanczos matrix
@@ -80,9 +87,14 @@ void partition(coord* A, size_t dim, size_t N, int comm_size, MPI_Comm comm ) {
     q[i] = (double*)malloc(sizeof(double)*dim);
 
   // initial guess
+  if (rank == 0)
+  {
   #pragma omp parallel for
   for(i=0;i<dim;i++)
     q[0][i] = (double)rand()/(double)RAND_MAX * 100;
+  }
+
+  MPI_Bcast(&q[0][0],dim, MPI_DOUBLE, 0, comm);
 
   // normalize initial guess
   b[0] = norm(q[0],dim);
@@ -137,13 +149,17 @@ void partition(coord* A, size_t dim, size_t N, int comm_size, MPI_Comm comm ) {
         q[j+1][i] = z[i]/b[j];
   }
   double end = omp_get_wtime();
-
   printf("time: %lf\n", end-begin);
+
+  free(diagonal);
+  free(scanned);
+  free(z);
+
+  double* V = (double*)malloc(sizeof(double)*ITER*ITER);// blank workspace for eig function
+  double* splitter = (double*)malloc(sizeof(double)*dim);
 
   // compute eigenvalues and eigenvectors of lanczos matrix
   eig(a,b,V,T_size);
-
-  double* splitter = (double*)malloc(sizeof(double)*dim);
 
   #pragma omp parallel for
   for(i=0;i<dim;i++)
@@ -153,6 +169,13 @@ void partition(coord* A, size_t dim, size_t N, int comm_size, MPI_Comm comm ) {
   for(i=0;i<dim;i++)
     for(j=0;j<T_size;j++)
       splitter[i] += q[j][i]*V[T_size + j];
+
+
+  free(V);
+
+  #pragma omp parallel for
+  for(i=0;i<ITER;i++)
+    free(q[i]);
 
 
   printf("splitter:\n");
@@ -165,15 +188,72 @@ void partition(coord* A, size_t dim, size_t N, int comm_size, MPI_Comm comm ) {
     printf("%lf\n\t", a[i]);
   printf("\n");
 
-  free(A);
-  free(diagonal);
-  free(scanned);
-  free(V);
-  free(z);
+  unsigned int AnewCount=0, newDim=0;
+
+  #pragma omp parallel for reduction(+:AnewCount)
+  for(i=0;i<N;i++)
+    if (color == 0 && splitter[A[i].row] >= 0 && splitter[A[i].col] >= 0)
+      AnewCount++;
+    else if (color == 1 && splitter[A[i].row] < 0 && splitter[A[i].col] < 0)
+      AnewCount++;
+
+  #pragma omp parallel for reduction(+:newDim)
+  for(i=0;i<dim;i++)
+    if (color == 0 && splitter[i] >= 0)
+      newDim++;
+    else if (color == 1 && splitter[i] < 0)
+      newDim++;
+
+  coord* Anew = (coord*)malloc(sizeof(coord)*AnewCount);
+
+  omp_lock_t writelock;
+  omp_init_lock(&writelock);
+
+  AnewCount=0;
+
+
+
 
   #pragma omp parallel for
-  for(i=0;i<ITER;i++)
-    free(q[i]);
+  for(j=0;j<N;j++)
+    if(color == 0 && splitter[A[j].row] >= 0 && splitter[A[j].col] >= 0){
+      omp_set_lock(&writelock);
+      Anew[AnewCount].row = A[j].row;
+      Anew[AnewCount].col = A[j].col;
+      AnewCount++;
+      omp_unset_lock(&writelock);
+    }
+    else if(color == 1 && splitter[A[j].row] < 0 && splitter[A[j].col] < 0){
+      omp_set_lock(&writelock);
+      Anew[AnewCount].row = A[j].row;
+      Anew[AnewCount].col = A[j].col;
+      AnewCount++;
+      omp_unset_lock(&writelock);
+    }
+
+  omp_destroy_lock(&writelock);
+
+  FILE* fp;
+
+  printf("%d %d\n", color, newDim);
+
+  if(color == 0)
+    fp = fopen("Matrices/Aplus.dat","w");
+  else
+    fp = fopen("Matrices/Aminus.dat","w");
+
+  for(i=0;i<AnewCount;i++)
+    fprintf(fp,"%d %d\n",Anew[i].row,Anew[i].col);
+
+
+
+  // MPI_Comm_split(comm, color, rank, &new_comm);
+  // partition(Anew,newDim,AnewCount,size,new_comm);
+
+
+  free(A);
+  free(Anew);
+  free(splitter);
 
   MPI_Finalize();
 }
