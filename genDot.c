a8002f6fe78c6e6a672d26c61c0ef82aa8cd4ab5
allywarner <allywarner@sci.utah.edu>
2016-04-25 17:18:59 -0600

    It's working! Kind of...
    It makes the edges a color, but I can’t quite figure out how to make
    the nodes smaller and a filled color. It will only fill 3/5. It will do
    the large matrix, but the size of the nodes are way too big!

--- genDot.c
+++ genDot.c
@@ -1,37 +1,52 @@
 //Generates the dot file to view in graphviz
 
-#include "lanczos.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
-char* colors[] = {"green","red","blue","purple","yellow","pink","brown"};
+//Initializes 16 colors. These can be changed to anything and I tried to pick colors that look different enough! More colors can be found at http://www.graphviz.org/doc/info/colors.html if you don't like them!
+char* colors[] = {"green","red","blue","purple","yellow","magenta","brown","black","orange","skyblue1","forestgreen","slateblue1","wheat4","lightpink2","ivory4","cyan"};
 
+//Generates the dot file with either one color or no color, we can easily change this if we are getting a file with several processors worth of information.
 void genDot(char* data,int colorNum){
     
-    FILE* dotFile,readFile;
+    //Initializes files
+    FILE* dotFile;
+    FILE* readFile;
     
+    //Opens the file to read and write
     readFile = fopen(data,"r");
     dotFile = fopen("dotFile.gv","w");
     
-    fprintf(dotFile,"graph {");
+    //Writes the first line
+    fprintf(dotFile,"graph {fixedsize=true\n");
     
+    //Gets rid of the first line in the file we are reading
+    fscanf(readFile,"%*[^\n]\n",NULL);
+    
+    //reads and writes until the end of the read file, writes a color if the input is not 0
     int node1,node2;
     while(fscanf(readFile,"%d %d\n",&node1,&node2) != EOF) {
         if (colorNum == 0) {
-        fprintf(dotFile,"%d -- %d;\n",node1,node2);
+            fprintf(dotFile,"%d -- %d;\n",node1,node2);
         } else {
-            fprintf(dotFile,"%d -- %d [color = %s];",node1,node2,colors[colorNum-1])
+            fprintf(dotFile,"%d -- %d;\nnode[shape=circle,color = %s,style=filled,height=0.01,width=0.01];\n",node1,node2,colors[colorNum-1]);
         }
     }
+    //writes the last line
     fprintf(dotFile,"}");
     
+    //closes the files
     fclose(readFile);
     fclose(dotFile);
 }
 
-//This main is for debugging
+////This main is for debugging
 int main(int argc, char* argv[]) {
     
     if (argc < 2){
-        cerr << "Error.";
+        printf("Error.");
         return 1;
     }
     
